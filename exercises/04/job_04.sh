a7a4345358fcbf7414c82b6128cf58af10c4b832
LV mit LCC <cb761225@login.lcc3.uibk.ac.at>
2024-04-15 23:35:28 +0200

    job script for 1 and massif

--- exercises/04/job_04.sh
+++ exercises/04/job_04.sh
@@ -3,7 +3,7 @@
 # Execute job in the partition "lva" unless you have special requirements.
 #SBATCH --partition=lva
 # Name your job to be able to identify it later
-#SBATCH --job-name test
+#SBATCH --job-name 04fa
 # Redirect output stream to this file
 #SBATCH --output=output.log
 # Maximum number of tasks (=processes) to start in total
@@ -13,4 +13,16 @@
 # Enforce exclusive node allocation, do not share with other jobs
 #SBATCH --exclusive
 
-/bin/hostname
+echo "#########################################"
+echo "################ ssca2 ##################"
+echo "#########################################"
+valgrind --tool=massif --massif-out-file="massif-ssca2.out" ./ssca2 17
+
+echo
+echo
+echo
+echo "#########################################"
+echo "############### npb_bt_a #################"
+echo "#########################################"
+valgrind --tool=massif --massif-out-file="massif-npb_bt_a.out" ./npb_bt_a
+
