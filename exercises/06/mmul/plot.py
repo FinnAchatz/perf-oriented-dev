9a88164ca5515e87cc820b3cc0991175d8d36d8c
Finn Achatz <finn.achatz@student.uibk.ac.at>
2024-05-06 13:31:37 +0200

    added more plots

--- exercises/06/mmul/plot.py
+++ exercises/06/mmul/plot.py
@@ -2,13 +2,18 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import itertools
+from scipy import stats
 
 df = pd.read_csv("times.csv")
 
-
 # grouped = df.groupby(["init","mult","size_x","size_y"])['time'].mean().reset_index()
-grouped = df.groupby(["init","mult","size_x","size_y"]).time.agg({'median', 'mean', 'std'}).reset_index()
+# grouped = df.groupby(["init","mult","size_x","size_y"]).time.nlargest(n=2, keep='last').nsmallest(n=2, keep='last').agg({'median', 'mean', 'std'}).reset_index()
+df_2 = df[df.groupby(["init","mult","size_x","size_y"])['time'].transform(stats.zscore).abs() < 3]
+#
+grouped = df_2.groupby(["init","mult","size_x","size_y"]).time.agg({'median', 'mean', 'std', 'min', 'max'}).reset_index()
 grouped["text"] = ["M$=${0:.2f}\n$\mu=${1:.2f}\n$\sigma=${2:.2f}".format(med,mean,std) for med,mean,std in zip(grouped["median"],grouped["mean"], grouped["std"])]
+
+print(grouped.describe())
 
 ref = df[df["name"]=="FalseFalseNoneNone"]['time'].mean()
 std = df[df["name"]=="FalseFalseNoneNone"]['time'].std()
@@ -17,6 +22,8 @@ std = df[df["name"]=="FalseFalseNoneNone"]['time'].std()
 print("reference: mean =", ref, "; std =", std)
 print(grouped.sort_values(by="mean"))
 print(grouped.sort_values(by="median"))
+
+###### PLOT HEATMAPS #######
 
 for version, col in itertools.product(["mult", "init", "both"], ["mean", "median"]):
      
@@ -30,44 +37,22 @@ for version, col in itertools.product(["mult", "init", "both"], ["mean", "median
     pivot_df = filtered_df.pivot(index='size_x', columns='size_y', values=col)
 
     pivot_df_text = filtered_df.pivot(index='size_x', columns='size_y', values='text').to_numpy()
+
+    cmap = 'rocket' if col == 'mean' else 'mako' 
     
     # Create the heatmap
     plt.figure(figsize=(10, 8))
-    sns.heatmap(pivot_df, cmap='YlGnBu', annot=pivot_df_text, fmt="", vmin=71, vmax=75)
+    sns.heatmap(pivot_df, cmap=cmap, annot=pivot_df_text, fmt="", vmin=71.5, vmax=73)
     plt.title(f'Heatmap of {col} time (Tiling of {version})')
     plt.savefig(f"{version}_{col}.png")
     plt.show()
 
 
+###### PLOT BARS OF TOP #######
 
-# ##### Init
-#
-# # Filter the DataFrame
-# filtered_df = grouped[(grouped['init'] != False) & (grouped['mult'] != True)]
-#
-# # Pivot the DataFrame to get the values for the heatmap
-# pivot_df = filtered_df.pivot(index='size_x', columns='size_y', values='time')
-#
-# # Create the heatmap
-# plt.figure(figsize=(10, 8))
-# sns.heatmap(pivot_df, cmap='YlGnBu', annot=True, fmt=".1f", vmin=71, vmax=75)
-# plt.title('Heatmap of Mean Time (Tiling of Init)')
-# plt.savefig("init.png")
-# plt.show()
-#
-#
-#
-# ##### Both
-#
-# # Filter the DataFrame
-# filtered_df = grouped[(grouped['init'] != False) & (grouped['mult'] != False)]
-#
-# # Pivot the DataFrame to get the values for the heatmap
-# pivot_df = filtered_df.pivot(index='size_x', columns='size_y', values='time')
-#
-# # Create the heatmap
-# plt.figure(figsize=(10, 8))
-# sns.heatmap(pivot_df, cmap='YlGnBu', annot=True, fmt=".1f", vmin=71, vmax=75)
-# plt.title('Heatmap of Mean Time (Tiling of Both)')
-# plt.savefig("both.png")
-# plt.show()
+for mode in ['mean', 'median', 'min', 'max']:
+    top = grouped.sort_values(by=mode).head(10)
+    top['name'] = [f"{'init ' if init else ''}{'mult ' if mult else ''}{int(x)}x{int(y)}" for init,mult,x,y in zip(top['init'], top['mult'], top['size_x'], top['size_y'])]
+    ax = top.plot.bar(x='name', y=mode, rot=25, ylim=(top[mode].iloc[0]-0.1, top[mode].iloc[9]+0.1), figsize=(10,8))
+    plt.savefig(f"top_{mode}s.png")
+    plt.show()
