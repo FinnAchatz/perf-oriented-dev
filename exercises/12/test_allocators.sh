65bef091433fe04c449c481b4d42a27085447378
eprader <56026248+eprader@users.noreply.github.com>
2024-06-15 23:37:17 +0200

    update to use built lua path

--- exercises/12/test_allocators.sh
+++ exercises/12/test_allocators.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "testing rpmalloc"
-LDD_PRELOAD=./allocators/rpmalloc/bin/linux/release/x86-64/librpmalloc.so lua ../../lua/fib.lua
+LDD_PRELOAD=./allocators/rpmalloc/bin/linux/release/x86-64/librpmalloc.so ./lua-5.4.6/src/lua ../../lua/fib.lua
 
 echo "testing mimalloc"
-LDD_PRELOAD=./allocators/mimalloc/out/release/libmimalloc.so lua ../../lua/fib.lua
+LDD_PRELOAD=./allocators/mimalloc/out/release/libmimalloc.so ./lua-5.4.6/src/lua ../../lua/fib.lua
