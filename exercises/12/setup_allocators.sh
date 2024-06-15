2d5e75890fd44a61d650dba856cafdcec6b151b8
eprader <56026248+eprader@users.noreply.github.com>
2024-06-15 20:46:49 +0200

    first setup test on lcc

---
+++ exercises/12/setup_allocators.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+mkdir allocators && cd allocators
+
+echo -e "${GREEN}Cloning rpmalloc...${NC}"
+git clone git@github.com:mjansson/rpmalloc.git
+
+# Build rpmalloc
+echo -e "${GREEN}Building rpmalloc...${NC}"
+cd rpmalloc \
+    && ./configure.py \
+    && ninja
