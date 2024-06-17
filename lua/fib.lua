d795a924f52e8617456c14779d06c6d350fa7142
eprader <56026248+eprader@users.noreply.github.com>
2024-06-17 20:44:11 +0200

    formatting

--- lua/fib.lua
+++ lua/fib.lua
@@ -1,38 +1,37 @@
-
 function fibonacci_naive(n)
-    if n < 2 then
-        return n
-    end
-    return fibonacci_naive(n-1) + fibonacci_naive(n-2)
+	if n < 2 then
+		return n
+	end
+	return fibonacci_naive(n - 1) + fibonacci_naive(n - 2)
 end
 
 function fibonacci_tail(n)
-  local function inner(m, a, b)
-    if m == 0 then
-      return a
-    end
-    return inner(m-1, b, a+b)
-  end
-  return inner(n, 0, 1)
+	local function inner(m, a, b)
+		if m == 0 then
+			return a
+		end
+		return inner(m - 1, b, a + b)
+	end
+	return inner(n, 0, 1)
 end
 
 function fibonacci_iter(n)
-    local a, b = 0, 1
-    for i = 1, n do
-        a, b = b, a + b
-    end
-    return a
+	local a, b = 0, 1
+	for i = 1, n do
+		a, b = b, a + b
+	end
+	return a
 end
 
 function measure(reps, f, ...)
-  local start = os.clock()
-  local res
-  for i = 0, reps do
-    res = f(...)
-  end
-  local time = (os.clock() - start)
-  
-  return time, res
+	local start = os.clock()
+	local res
+	for i = 0, reps do
+		res = f(...)
+	end
+	local time = (os.clock() - start)
+
+	return time, res
 end
 
 t, v = measure(100, fibonacci_naive, 30)
